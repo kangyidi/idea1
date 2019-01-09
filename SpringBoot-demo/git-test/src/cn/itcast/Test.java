2f18b08b50b78cee76fef40c535c74a5c73b91cf
kangyidi <1643614030@qq.com>
2019-01-09 14:35:11 +0800

    打印a+b结果

--- SpringBoot-demo/git-test/src/cn/itcast/Test.java
+++ SpringBoot-demo/git-test/src/cn/itcast/Test.java
@@ -7,5 +7,12 @@ public class Test {
         System.out.println("test");
         System.out.println("test");
         System.out.println("test");
+        test();
+    }
+
+    static void test(){
+        double a=0.1;
+        double b=0.2;
+        System.out.println(a + b);
     }
 }
